94c420f471be99ca88cd524923a2f43364234a35
VolodymyrTrush <91078173+VolodymyrTrush@users.noreply.github.com>
2022-05-29 08:28:54 +0000

    Deploying to gh-pages from @ VolodymyrTrush/goit-js-hw-11@3c86601043150ec9dc0895b4d1eb089604db41ae 🚀

--- src.28346d86.js
+++ src.28346d86.js
@@ -99,4 +99,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{"./fetch-images":"Zzam","../templates/card-template.hbs":"XZJ2","notiflix/build/notiflix-notify-aio":"MeeU","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"clu1"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/search-images/index");
 },{"./sass/main.scss":"clu1","./js/search-images/index":"ZkNT"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.b168a627.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.28346d86.js.map
