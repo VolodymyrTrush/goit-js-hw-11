94c8f55803a6cbd766eb43c34f9469f2fdb4eecf
VolodymyrTrush <vmtrush@gmail.com>
2022-05-31 18:54:00 +0300

    HW11
    Task

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,20 +1,16 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '27183497-70bd3599297502793f5a9350a';
 const url = `${BASE_URL}?key=${API_KEY}`;
 
 export const fetchPicsOptions = {
-        q: '',
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true,
-        per_page: 40,
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
 };
 
-export const fetchPics = async(params) => await axios.get(url, {params}).catch((e)=>console.error(e))
-
-
-
-  
- 
+export const fetchPics = async params =>
+  await axios.get(url, { params }).catch(e => console.error(e));
